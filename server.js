d5612012872f883ea43e9242ad629c0a8f1e9802
Tanvir156 <72537009+Tanvir156@users.noreply.github.com>
2023-02-11 01:53:18 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://tic-tec-videochat.onrender.com");
+  res.header("Access-Control-Allow-Origin", "https://tictecvideochat2.onrender.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 const corsOptions ={
-    origin:'https://tic-tec-videochat.onrender.com', 
+    origin:'https://tictecvideochat2.onrender.com', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
@@ -21,7 +21,7 @@ const server = app.listen(5000, console.log("server is running at port 5000"));
 const io = require("socket.io")(server, {
   pingTimeout: 6000,
   cors: {
-    origin: "https://tic-tec-videochat.onrender.com",
+    origin: "https://tictecvideochat2.onrender.com",
   },
 });
 io.on("connection", (socket) => {
